50e13860cb3725d5c5e3c175b3d8d5322eef6145
지나가던 잉여 <producer82@naver.com>
2019-11-11 22:38:12 +0900

    연습

---
+++ DataFrame.R
@@ -0,0 +1,8 @@
+name <- c('Chika', 'Miyuki', 'Kaguya', 'Yuu')
+sex <- c('f', 'm', 'f', 'm')
+age <- c(18, 18, 18, 17)
+
+council <- data.frame(name, sex, age)
+
+council
+
---
+++ Variable.R
@@ -0,0 +1,9 @@
+x <- c(12, 2, 5, 6, 7, 20)
+y <- seq(from = 1, to = 20, by = 3)
+
+x
+y
+min(x)
+max(x)
+mean(y)
+which(x > 10)
